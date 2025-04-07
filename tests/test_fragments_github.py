a12e51bae49537ea3c93d53e73b3230a0e3c9737
Simon Willison <swillison@gmail.com>
2025-04-07 09:35:08 -0700

    Automated test, closes #2

--- tests/test_fragments_github.py
+++ tests/test_fragments_github.py
@@ -1,6 +1,15 @@
-from llm.plugins import pm
+from llm_fragments_github import github_loader
 
 
 def test_plugin_is_installed():
-    names = [mod.__name__ for mod in pm.get_plugins()]
-    assert "llm_fragments_github" in names
+    fragments = github_loader("simonw/test-repo-for-llm-fragments-github")
+    assert [(fragment.source, str(fragment)) for fragment in fragments] == [
+        (
+            "simonw/test-repo-for-llm-fragments-github/README.md",
+            "# test-repo-for-llm-fragments-github\nUsed by tests for https://github.com/simonw/llm-fragments-github\n",
+        ),
+        (
+            "simonw/test-repo-for-llm-fragments-github/example/file.txt",
+            "This is an example file.\n",
+        ),
+    ]
